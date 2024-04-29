e24fd5efba7b2470d66e70613c6e4c9157d9cdda
Decroッ <115628021+Decro2908@users.noreply.github.com>
2024-04-29 10:35:29 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
+// App.jsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [catData, setCatData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search', {
+        params: {
+          api_key: 'live_HBuuXBi8sFjlDp5BNLjAa0Dp83IJghXwwCaXIswcRObUgPc1W7L16kjhPFffkJnO',
+        },
+      });
+      setCatData(response.data[0]);
+    } catch (error) {
+      console.error('Error fetching cat data:', error);
+    }
+  };
+
+  const handleNextCat = () => {
+    fetchData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      <header>
+        <nav className="navbar">
+          <h1 className="navbar-title">Kočkyyyy</h1>
+        </nav>
       </header>
+      <main>
+        <div className="gallery-container">
+          <div className="gallery-title"></div>
+          <div className="cat-card">
+            {catData && <img src={catData.url} alt="Náhodná kočka" className="cat-image" />}
+          </div>
+          <button onClick={handleNextCat} className="next-button">Další</button>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
